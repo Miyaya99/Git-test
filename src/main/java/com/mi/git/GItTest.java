722e880785db1e8cf1c3b35729cd6d4114cec17a
Miyaya <Miyaya99@Mi.com>
2022-06-01 00:10:45 +0800

    three commitByIDEA

--- src/main/java/com/mi/git/GItTest.java
+++ src/main/java/com/mi/git/GItTest.java
@@ -10,5 +10,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git2");
+        System.out.println("hello Git3");
     }
 }
