beda709ed3a05069f70b9d5405d5e4277993961b
Miyaya <Miyaya99@Mi.com>
2022-06-01 00:06:38 +0800

    first commitByIDEA

---
+++ src/main/java/com/mi/git/GItTest.java
@@ -0,0 +1,13 @@
+package com.mi.git;
+
+/**
+ * Classname: GItTest
+ * Date: 2022/6/1 0:01
+ * Author: Mi
+ * Description:
+ */
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
